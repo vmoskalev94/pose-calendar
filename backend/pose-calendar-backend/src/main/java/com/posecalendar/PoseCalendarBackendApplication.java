59ac882e09a80a084fd8dfffd0fe67c8a5e3312c
vmoskalev <vladislav.moskalev@gmail.com>
2025-12-09 00:08:07 +0300

    git ignore check

--- backend/pose-calendar-backend/src/main/java/com/posecalendar/PoseCalendarBackendApplication.java
+++ backend/pose-calendar-backend/src/main/java/com/posecalendar/PoseCalendarBackendApplication.java
@@ -8,6 +8,7 @@ public class PoseCalendarBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PoseCalendarBackendApplication.class, args);
+        System.out.println("Pose calendar backend application started");
     }
 
 }
